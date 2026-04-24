c7c893498921c178fcd6c65685438c0270e46add
igweek <soscn@hotmail.com>
2026-04-24 21:55:47 +0800

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="h-full flex items-center justify-center bg-white rounded-3xl overflow-hidden shadow-sm border border-[#E5E5E7]">
       <img 
-        src="https://cdn.nodeimage.com/i/sVXW1HmAe4muUMsM1zat4gze25d6YyGH.webp"
+        src="https://cdn.nodeimage.com/i/gXZmqM19C6wrWgEzAcJ7rQskannFjH4b.webp"
         alt="首页展示"
         className="max-w-full max-h-full object-contain"
         referrerPolicy="no-referrer"
