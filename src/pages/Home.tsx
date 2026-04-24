a1caa91dbdc77dc9481d7fb23bb2e65fcbeb612d
igweek <soscn@hotmail.com>
2026-04-24 22:15:05 +0800

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <div className="h-full flex items-center justify-center bg-white rounded-3xl overflow-hidden shadow-sm border border-[#E5E5E7]">
+    <div className="h-full flex items-center justify-center">
       <img 
-        src="https://cdn.nodeimage.com/i/gXZmqM19C6wrWgEzAcJ7rQskannFjH4b.webp"
+        src="https://cdn.nodeimage.com/i/lDk7ft669DDplmzXJWEUclFUqVZWbQsU.webp"
         alt="首页展示"
         className="max-w-full max-h-full object-contain"
         referrerPolicy="no-referrer"
