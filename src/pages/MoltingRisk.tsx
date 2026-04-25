1c1a96074e137e3cf7481d527b8d9a2fb6133d1e
igweek <soscn@hotmail.com>
2026-04-25 14:53:56 +0800

    Update MoltingRisk.tsx

--- src/pages/MoltingRisk.tsx
+++ src/pages/MoltingRisk.tsx
@@ -251,7 +251,7 @@ export default function MoltingRisk() {
   };
 
   return (
-    <div className="space-y-6 max-w-7xl mx-auto">
+    <div className="space-y-6 max-w-7xl mx-auto p-8">
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-8">
         {/* Logic Gates */}
         <div className="lg:col-span-4 space-y-6">
